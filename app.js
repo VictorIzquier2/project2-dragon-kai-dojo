72e488788dbbda223d6334024a7f7cee6e63e62e
VictorIzquier2 <victorizquier2@WorkStation.localdomain>
2020-11-14 17:28:33 +0100

    dotenv & express

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('Hello there! Node is installed!');
+// REQUIRES
+const chalk = require('chalk');
+const dotenv = require('dotenv');
+const express = require('express');
+
+
+// CONSTANTS
+const app = express();
+
+// .env CONFIG
+dotenv.config();
+
+console.log(chalk.blue('Hello there! Node is installed!'));
+
+// LISTENER
+app.listen(process.env.PORT, () => {
+  const PORT = process.env.PORT;
+  console.log(chalk.green.inverse.bold(`Connected to port: ${PORT}`));
+})
