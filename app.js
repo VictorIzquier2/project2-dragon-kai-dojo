b028f4f40cd9f88715475b4aff6ef741c256d8bc
VictorIzquier2 <victorizquier2@WorkStation.localdomain>
2020-11-18 21:43:51 +0100

    Battles done

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const Karateka = require('./models/Karateka.js');
 const Master = require('./models/Master.js');
 const Sensei = require('./models/Sensei.js');
 const { level } = require('chalk');
+const { findOneAndUpdate } = require('./models/Civilian.js');
 // .env CONFIG
 dotenv.config();
 
@@ -103,22 +104,22 @@ app.post('/train', (req, res, next) => {
       let levelUp = 0;
       let numberOfDices = trainee.dexterity + trainee.strength;
       let diceRoll = numberOfDices * Math.floor(Math.random() * (11-2)) + 1;
-      if (numberOfDices == 2){
-        diceRoll > 16 ? levelUp = 1 : levelUp = 0;
+      if (numberOfDices == 2 && trainee.strength < 5){
+        diceRoll > 17 ? levelUp = 1 : levelUp = 0;
       }else if(numberOfDices == 3){
-        diceRoll > 24 ? levelUp = 1 : levelUp = 0;
+        diceRoll > 25 ? levelUp = 1 : levelUp = 0;
       }else if(numberOfDices == 4){
-        diceRoll > 32 ? levelUp = 1 : levelUp = 0;
+        diceRoll > 33 ? levelUp = 1 : levelUp = 0;
       }else if(numberOfDices == 5){
-        diceRoll > 40 ? levelUp = 1 : levelUp = 0;
+        diceRoll > 41 ? levelUp = 1 : levelUp = 0;
       }else if(numberOfDices == 6){
-        diceRoll > 48 ? levelUp = 1 : levelUp = 0;
+        diceRoll > 49 ? levelUp = 1 : levelUp = 0;
       }else if(numberOfDices == 7){
-        diceRoll > 56 ? levelUp = 1 : levelUp = 0;
+        diceRoll > 57 ? levelUp = 1 : levelUp = 0;
       }else if(numberOfDices == 8){
-        diceRoll > 64 ? levelUp = 1 : levelUp = 0;
+        diceRoll > 65 ? levelUp = 1 : levelUp = 0;
       }else if(numberOfDices == 9){
-        diceRoll > 72 ? levelUp = 1 : levelUp = 0;
+        diceRoll > 73 ? levelUp = 1 : levelUp = 0;
       }
       return levelUp;    
     }
@@ -127,24 +128,24 @@ app.post('/train', (req, res, next) => {
       let levelUp = 0;
       let numberOfDices = trainee.dexterity + trainee.dexterity - Math.round(trainee.dexterity/2);
       let diceRoll = numberOfDices * Math.floor(Math.random() * (11-2)) + 1;
-      if(numberOfDices == 1){  
+      if(numberOfDices == 1 < trainee.dexterity < 5){  
         diceRoll > 8 ? levelUp = 1 : levelUp = 0;
       }else if (numberOfDices == 2){
-        diceRoll > 16 ? levelUp = 1 : levelUp = 0;
+        diceRoll > 17 ? levelUp = 1 : levelUp = 0;
       }else if(numberOfDices == 3){
-        diceRoll > 24 ? levelUp = 1 : levelUp = 0;
+        diceRoll > 25 ? levelUp = 1 : levelUp = 0;
       }else if(numberOfDices == 4){
-        diceRoll > 32 ? levelUp = 1 : levelUp = 0;
+        diceRoll > 33 ? levelUp = 1 : levelUp = 0;
       }else if(numberOfDices == 5){
-        diceRoll > 40 ? levelUp = 1 : levelUp = 0;
+        diceRoll > 41 ? levelUp = 1 : levelUp = 0;
       }else if(numberOfDices == 6){
-        diceRoll > 48 ? levelUp = 1 : levelUp = 0;
+        diceRoll > 49 ? levelUp = 1 : levelUp = 0;
       }else if(numberOfDices == 7){
-        diceRoll > 56 ? levelUp = 1 : levelUp = 0;
+        diceRoll > 57 ? levelUp = 1 : levelUp = 0;
       }else if(numberOfDices == 8){
-        diceRoll > 64 ? levelUp = 1 : levelUp = 0;
+        diceRoll > 65 ? levelUp = 1 : levelUp = 0;
       }else if(numberOfDices == 9){
-        diceRoll > 72 ? levelUp = 1 : levelUp = 0;
+        diceRoll > 73 ? levelUp = 1 : levelUp = 0;
       }
       return levelUp;
     }
@@ -153,28 +154,27 @@ app.post('/train', (req, res, next) => {
       let levelUp = 0;
       let numberOfDices = trainee.dexterity + trainee.stamina;
       let diceRoll = numberOfDices * Math.floor(Math.random() * (11-2)) + 1;
-      if (numberOfDices == 2){
-        diceRoll > 16 ? levelUp = 1 : levelUp = 0;
-        return levelUp; 
+      if (numberOfDices == 2 && trainee.stamina < 5){
+        diceRoll > 17 ? levelUp = 1 : levelUp = 0;
       }else if(numberOfDices == 3){
-        diceRoll > 24 ? levelUp = 1 : levelUp = 0;
+        diceRoll > 25 ? levelUp = 1 : levelUp = 0;
       }else if(numberOfDices == 4){
-        diceRoll > 32 ? levelUp = 1 : levelUp = 0;
+        diceRoll > 33 ? levelUp = 1 : levelUp = 0;
       }else if(numberOfDices == 5){
-        diceRoll > 40 ? levelUp = 1 : levelUp = 0;
+        diceRoll > 41 ? levelUp = 1 : levelUp = 0;
       }else if(numberOfDices == 6){
-        diceRoll > 48 ? levelUp = 1 : levelUp = 0;
+        diceRoll > 49 ? levelUp = 1 : levelUp = 0;
       }else if(numberOfDices == 7){
-        diceRoll > 56 ? levelUp = 1 : levelUp = 0;
+        diceRoll > 57 ? levelUp = 1 : levelUp = 0;
       }else if(numberOfDices == 8){
-        diceRoll > 64 ? levelUp = 1 : levelUp = 0;
+        diceRoll > 65 ? levelUp = 1 : levelUp = 0;
       }else if(numberOfDices == 9){
-        diceRoll > 72 ? levelUp = 1 : levelUp = 0;
+        diceRoll > 73 ? levelUp = 1 : levelUp = 0;
       }  
       return levelUp;   
     }
 
-    Karateka.find({}, {name: 1, imageUrl: 1, strength: 1, dexterity: 1, stamina: 1, _id: 1})
+    Karateka.find({}, {name: 1, imageUrl: 1, strength: 1, dexterity: 1, stamina: 1, _id: 1}).sort({strength: -1, dexterity: -1, stamina: -1})
       .then((trainees) => {
         trainees.forEach((trainee)=>{
           const id = trainee._id;
@@ -182,9 +182,6 @@ app.post('/train', (req, res, next) => {
           const newDexterity = trainee.dexterity += levelUpDexterity(trainee);
           const newStamina = trainee.stamina += levelUpStamina(trainee);
           Karateka.findByIdAndUpdate(id, {strength: newStrength, dexterity: newDexterity, stamina: newStamina})
-          .then((trainee)=>{
-            console.log(trainee);
-          })
         })
         res.render('train', {trainees})
 
@@ -195,7 +192,7 @@ app.post('/train', (req, res, next) => {
   });
 
 app.get('/battle', (req, res, next) => {
-  Karateka.find({}, {name: 1, imageUrl: 1, strength: 1, dexterity: 1, stamina: 1, level: 1, nature: 1, mana: 1, _id: 1})
+  Karateka.find({}, {name: 1, imageUrl: 1, strength: 1, dexterity: 1, stamina: 1, level: 1, nature: 1, mana: 1, standing: 1, _id: 1}).sort({strength: -1, dexterity: -1, stamina: -1})
       .then((opponents) => {
         res.render('battle', ({opponents}))
       })
@@ -204,12 +201,76 @@ app.get('/battle', (req, res, next) => {
       })
 });
 
-app.post('/battle', (req, res, next) => {
-  console.log(req.body);
+app.post('/battle/', (req, res, next) => {
+    const findFirstOne = () => {
+      return Karateka.findById(req.body._id[0])
+    }
+  
+    const findSecondOne = () => {
+      return Karateka.findById(req.body._id[1])
+    }
+    Promise.all([findFirstOne(), findSecondOne()])
+      .then((result) => {
+        const firstStrike = (aggressor, defensor) => {
+          let aggNumberOfDices = aggressor.dexterity + aggressor.strength;
+          let aggDiceRoll = aggNumberOfDices * Math.floor(Math.random() * (11-2)) + 1;
+          let defNumberOfDices = defensor.dexterity + defensor.stamina;
+          let defDiceRoll = defNumberOfDices * Math.floor(Math.random() * (11-2)) + 1;
+          aggDiceRoll < defDiceRoll ?  defensor.mana  : defensor.mana -= 1;
+        };
+
+        const strikes = (fighter1, fighter2) => {
+          let fighter1Luky = fighter1.dexterity * Math.floor(Math.random() * (11-2)) + 1;
+          let fighter2Luky = fighter2.dexterity * Math.floor(Math.random() * (11-2)) + 1;
+          fighter1Luky > fighter2Luky ? firstStrike(fighter1, fighter2) : firstStrike(fighter2, fighter1);
+        };
+
+        let winner = [];
+        let loser = [];
+
+        // The most aggressive strikes first
+        result[0].nature == 'wroth' ? firstStrike(result[0], result[1]) : firstStrike(result[1], result[0]);
+
+        // Rest of combat
+        do {strikes(result[0], result[1])} while(result[0].mana > 0 && result[1].mana > 0);
+
+        // Check Winner
+        result.forEach((opp)=> {
+          if(opp.mana > 0 ){
+            winner.push(opp);
+          }else {
+            loser.push(opp);
+          }
+        })
+        loser.forEach((opp)=>{
+          let oppId = opp._id;
+          const standingMinus = (opp) => opp.standing > 7 ? opp.standing -1 : opp.standing;
+          Karateka.findByIdAndUpdate(oppId, {standing : standingMinus(opp)})
+            .then(()=>{
+              winner.forEach((opp)=>{
+                let oppWId = opp._id;
+                const standingPlus = (opp) => opp.standing + 1;
+                Karateka.findByIdAndUpdate(oppWId, {standing: standingPlus(opp)})
+                  .then(()=> {
+                    // Hide BattleBtn and Choose Opponents
+                    let hide = "display: none"; 
+                    // Render Winner
+                    res.render('battle', {winner, hide});
+                  })
+              })
+          })
+        })
+        
+      })
+      .catch((err)=> {
+        console.log(err);
+        res.send("Error al renderizar la pelea")
+      })
+
 })
 
-app.post('/tourney', (req, res, next) => {
-  Karateka.find({}, {name: 1, imageUrl: 1, level: 1, standing: 1})
+app.get('/tourney', (req, res, next) => {
+  Karateka.find({}, {name: 1, imageUrl: 1, level: 1, mana: 1})
     .then((trainees) => {
       res.render('tourney', {trainees});
     })
@@ -218,6 +279,74 @@ app.post('/tourney', (req, res, next) => {
 
     });
 });
+
+app.post('/tourney', (req, res, next) => {
+  
+  Karateka.aggregate([{
+        $bucketAuto: {
+          groupBy: "$_id",
+          buckets: 6
+        }
+      }])
+    .then((result)=> {
+      result.forEach((couple)=>{
+        const firstOne = () => {
+          return Karateka.findById(couple._id.min)
+        }
+  
+        const secondOne = () => {
+          return Karateka.findById(couple._id.max)
+        }
+        Promise.all([firstOne(), secondOne()])
+          .then((result)=> {
+            const firstStrike = (aggressor, defensor) => {
+              let aggNumberOfDices = aggressor.dexterity + aggressor.strength;
+              let aggDiceRoll = aggNumberOfDices * Math.floor(Math.random() * (11-2)) + 1;
+              let defNumberOfDices = defensor.dexterity + defensor.stamina;
+              let defDiceRoll = defNumberOfDices * Math.floor(Math.random() * (11-2)) + 1;
+              aggDiceRoll < defDiceRoll ?  defensor.mana  : defensor.mana -= 1;
+            };
+
+            const strikes = (fighter1, fighter2) => {
+              let fighter1Luky = fighter1.dexterity * Math.floor(Math.random() * (11-2)) + 1;
+              let fighter2Luky = fighter2.dexterity * Math.floor(Math.random() * (11-2)) + 1;
+              fighter1Luky > fighter2Luky ? firstStrike(fighter1, fighter2) : firstStrike(fighter2, fighter1);
+            };
+
+            let winner = [];
+            let loser = [];
+
+            // The most aggressive strikes first
+            result[0].nature == 'wroth' ? firstStrike(result[0], result[1]) : firstStrike(result[1], result[0]);
+
+            // Rest of combat
+            do {strikes(result[0], result[1])} while(result[0].mana > 0 && result[1].mana > 0);
+
+            // Check Winner
+            result.forEach((opp)=> {
+              if(opp.mana > 0 ){
+                winner.push(opp);
+              }else {
+                loser.push(opp);
+              }
+            })
+            loser.forEach((defeated)=>{
+              let defeatedId = defeated._id;
+              const standingLess = (def) => def.standing > 7 ? def.standing -1 : def.standing;
+              Karateka.findByIdAndUpdate(defeatedId, {standing : standingLess(defeated)})
+              console.log(defeated);
+            })
+            
+          })
+        })
+      })
+      
+      // Hide TourneyBtn
+      let hide = "display: none";
+      // Render Tourney
+      res.render('tourney', {hide});
+      
+})
 
 app.get('/city', (req, res, next) => {
   
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,1 +1,27 @@
-console.log('running js!');
+// Battle //
+
+//Two opponents only
+let oppSelected = 0;
+const countOppSelected = (checkbox) => {
+  if(checkbox.checked == false){
+    oppSelected--;
+  }
+  if(checkbox.checked == true && oppSelected < 2){
+    oppSelected++;
+  }else{
+    checkbox.checked = false;
+  }
+  return oppSelected;
+}
+
+// Enable START BATTLE BUTTON
+const starBattleBtn = document.querySelector('.btn-battle');
+const enableBtn = () => {
+  if(oppSelected < 2){
+    starBattleBtn.setAttribute('disabled', false);
+    setTimeout(()=>{
+      starBattleBtn.removeAttribute('disabled');
+    }, 1000)
+  }
+}
+
