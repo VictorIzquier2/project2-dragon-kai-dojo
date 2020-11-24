b50d1663f560e245978e9376fe624c44569c7e51
VictorIzquier2 <victorizquier2@WorkStation.localdomain>
2020-11-24 22:18:15 +0100

    Master randomSkills

--- app.js
+++ app.js
@@ -170,10 +170,10 @@ app.post('/sign-up', (req, res, next) => {
                               return levelList[Math.round(Math.random() * (levelList.length))];
                             }
                             const randomSkill = () => {
-                              return Math.ceil(Math.random() * 5);
+                              return Math.floor(Math.random() * (6 - 3) + 3);
                             }
                             const randomMana = () => {
-                              return Math.ceil(Math.random() * 10);
+                              return Math.floor(Math.random() * (11 - 5) + 5);
                             }
                             const randomStanding = () => {
                               return Math.floor(Math.random() * (81 - 30 ) + 30);
